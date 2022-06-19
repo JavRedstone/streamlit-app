47c6e0fea29efdd3b9206f44a3956103d8eafa47
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2022-06-19 16:07:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-   🥑🍞
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
