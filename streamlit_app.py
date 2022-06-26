ae89cff520280f9e864736fd005f9374c0b59d0e
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2022-06-26 08:14:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-streamlit.title('My Parents' New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
