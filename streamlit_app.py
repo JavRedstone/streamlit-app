b941edbfea0952636601dd78b15a52ab9c4604cb
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2022-06-26 08:15:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Take the JSON version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output it to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
