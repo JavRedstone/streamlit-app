b817630e76fefa08adf855df31007c2bcbbccbd1
JavRedstone <85136616+JavRedstone@users.noreply.github.com>
2022-06-27 13:26:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,17 +43,25 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.header('The fruit load list contains:')
+streamlit.header('View Our Fruit List - Add Your Favourites!')
 # Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute('SELECT * FROM FRUIT_LOAD_LIST')
     return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('" + new_fruit + "')")
+    return "Thanks for adding " + new_fruit
+
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.write('Thanks for adding ', back_from_function)
